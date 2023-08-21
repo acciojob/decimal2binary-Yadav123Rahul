04765b2c95987286a69b4b4e75887b006249dfd9
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-08-22 01:12:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	 let s = "";
+    while (num > 0) {
+        if (num % 2 !== 0)
+			s += "1";
+        else s += "0";
+         num = Math.floor(num / 2);
+    }
+
+    return s.split("").reverse().join(""); 
+
+
   
 }
 
